5cef0d69f439de4fdef79960aaae38289fcdf1e8
stkey1 <st_key@hotmail.com>
2023-02-22 14:20:22 +0300

    Html, Css and JavaScript

---
+++ src/main/java/js/function.js
@@ -0,0 +1,8 @@
+kaydet.onclick = function(){kaydetti()};
+function kaydetti(){
+alert("Kayıt Başarılı")
+document.getElementById("kaydet").style.color = "red";
+}
+sil.onClick = function(){
+form1.reset;
+}
